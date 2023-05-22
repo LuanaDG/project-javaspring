7990e10b007a2ce8371a2643850cfab22376b1c3
Luana Dela Giustina <luanadelagiustina@gmail.com>
2023-05-22 13:03:52 -0300

    ajuste de exceções em Categoria

--- src/main/java/com/luana/services/CategoriaService.java
+++ src/main/java/com/luana/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.luana.services;
 
 import com.luana.entities.Categoria;
 import com.luana.repositories.CategoriaRepository;
+import com.luana.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,8 @@ public class CategoriaService {
 
     public Categoria findById(Integer id){
        Optional <Categoria> result = categoriaRepository.findById(id);
-        return result.orElse(null);
+        return result.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + "," +
+                        " Tipo: " + Categoria.class.getName()));
     }
 }
---
+++ src/main/java/com/luana/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.luana.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+    public ObjectNotFoundException(String msg, Throwable cause){
+        super(msg);
+    }
+}
