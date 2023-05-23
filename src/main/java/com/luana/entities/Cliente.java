020a7f52f53c251abea1e3984a80671a7522dd9e
Luana Dela Giustina <luanadelagiustina@gmail.com>
2023-05-23 11:11:57 -0300

    Cliente e Endereco

---
+++ src/main/java/com/luana/entities/Cliente.java
@@ -0,0 +1,104 @@
+package com.luana.entities;
+
+import com.luana.entities.enums.TipoCliente;
+import jakarta.persistence.*;
+
+import java.util.*;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String email;
+    private String cpfOuCnpj;
+    private Integer tipo;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Endereco> enderecos = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(name="TELEFONE")
+    private Set<String> telefones = new HashSet<>();
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpfOuCnpj = cpfOuCnpj;
+        this.tipo = tipo.getCod();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpfOuCnpj() {
+        return cpfOuCnpj;
+    }
+
+    public void setCpfOuCnpj(String cpfOuCnpj) {
+        this.cpfOuCnpj = cpfOuCnpj;
+    }
+
+    public TipoCliente getTipo() {
+        return TipoCliente.toEnum(tipo);
+    }
+
+    public void setTipo(TipoCliente tipo) {
+        this.tipo = tipo.getCod();
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public Set<String> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(Set<String> telefones) {
+        this.telefones = telefones;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cliente cliente)) return false;
+        return Objects.equals(id, cliente.id) && Objects.equals(nome, cliente.nome) && Objects.equals(email, cliente.email) && Objects.equals(cpfOuCnpj, cliente.cpfOuCnpj) && Objects.equals(tipo, cliente.tipo) && Objects.equals(enderecos, cliente.enderecos) && Objects.equals(telefones, cliente.telefones);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, email, cpfOuCnpj, tipo, enderecos, telefones);
+    }
+}
