a7d8ecfe9ed2fb99aa7936436fec9f07aba433c0
Luana Dela Giustina <luanadelagiustina@gmail.com>
2023-05-24 18:03:56 -0300

    POST categoria

--- src/main/java/com/luana/resources/CategoriaResource.java
+++ src/main/java/com/luana/resources/CategoriaResource.java
@@ -5,7 +5,9 @@ import com.luana.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,12 +17,18 @@ public class CategoriaResource {
 
     @Autowired
     private CategoriaService categoriaService;
+
    // @RequestMapping(value="/{id}", method = RequestMethod.GET)
     @GetMapping(value = "/{id}")
     public ResponseEntity<?> find (@PathVariable Integer id){
         Categoria obj = categoriaService.findById(id);
             return ResponseEntity.ok().body(obj);
-
-
+    }
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+        obj = categoriaService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
