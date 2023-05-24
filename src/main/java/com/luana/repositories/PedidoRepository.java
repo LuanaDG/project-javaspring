ef5e8473b7f443dbb4e746384bf96e69b79f9038
Luana Dela Giustina <luanadelagiustina@gmail.com>
2023-05-24 09:19:21 -0300

    pedido/pagamento

---
+++ src/main/java/com/luana/repositories/PedidoRepository.java
@@ -0,0 +1,10 @@
+package com.luana.repositories;
+
+import com.luana.entities.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
