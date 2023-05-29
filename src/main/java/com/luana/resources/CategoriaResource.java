c5efcda67ca8426ed6cd9ec85d66fc9b36fa5aa0
Luana Dela Giustina <luanadelagiustina@gmail.com>
2023-05-29 12:06:02 -0300

    paginacao

--- src/main/java/com/luana/resources/CategoriaResource.java
+++ src/main/java/com/luana/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import com.luana.dto.CategoriaDTO;
 import com.luana.entities.Categoria;
 import com.luana.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -49,6 +50,16 @@ public class CategoriaResource {
     public ResponseEntity<List<CategoriaDTO>> findAll (){
         List<Categoria> list = categoriaService.findAll();
         List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
+    @GetMapping(value = "/page")
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome")String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC")String direction){
+        Page<Categoria> list = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
         return ResponseEntity.ok().body(listDto);
     }
 }
--- src/main/java/com/luana/services/CategoriaService.java
+++ src/main/java/com/luana/services/CategoriaService.java
@@ -6,6 +6,9 @@ import com.luana.services.exceptions.DataIntegrityException;
 import com.luana.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,5 +48,9 @@ public class CategoriaService {
     }
     public List<Categoria> findAll(){
         return categoriaRepository.findAll();
+    }
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return categoriaRepository.findAll(pageRequest);
     }
 }
